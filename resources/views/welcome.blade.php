7c620aab3b5ade75dec41dbcfe8a01e885bb2906
MetTRocK <mettrock1@gmail.com>
2024-05-17 00:00:04 +0300

    00:00

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Merhaba</title>
+        <title>Naaaaber</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
