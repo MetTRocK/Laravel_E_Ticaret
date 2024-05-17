983d0af7a2bfbf6a9b216b2e47f86f6f0f88a1cb
MetTRocK <ggurberk@gmail.com>
2024-05-17 23:05:41 +0300

    .

--- app/Http/Controllers/Frontend/PageHomeController.php
+++ app/Http/Controllers/Frontend/PageHomeController.php
@@ -8,16 +8,14 @@ use App\Models\Slider;
 use App\Http\Controllers\Controller;
 use App\Models\About;
 use App\Models\Category;
-use App\Models\ulasim;
 
 class PageHomeController extends Controller
 {
     public function index(){
         $slider = Slider::where('status','1')->first();
-        $ulas = ulasim::first();
         $categories = Category::where('status','1')->get();
         $about = About::where('id',1)-> first();
 
-        return view('Frontend.Pages.index',compact('slider','ulas','categories','about'));
+        return view('Frontend.Pages.index',compact('slider','categories','about'));
     }
 }
